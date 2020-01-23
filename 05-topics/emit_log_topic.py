7558740ca28d8d5b07f3e7c13743fbbfc156f24a
Charles-Antoine Francisco <charlantfr@gmail.com>
2020-01-22 19:32:51 -0500

    Topics

---
+++ 05-topics/emit_log_topic.py
@@ -0,0 +1,19 @@
+import pika
+import sys
+
+
+if __name__ == "__main__":
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(host="localhost")
+    )
+    channel = connection.channel()
+
+    channel.exchange_declare(exchange="topic_logs", exchange_type="topic")
+
+    routing_key = sys.argv[1] if len(sys.argv) > 2 else "anonymous.info"
+    message = " ".join(sys.argv[2:]) or "Hello World!"
+    channel.basic_publish(
+        exchange="topic_logs", routing_key=routing_key, body=message
+    )
+    print(" [x] Sent {}:{}".format(routing_key, message))
+    connection.close()
